661ac53d4c1e1b01403ad1722b6154e9283d26c2
edmond-nader <49365047+edmond-nader@users.noreply.github.com>
2026-04-02 16:17:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var DAY_URLS = {
   'Wednesday 1': 'YOUR_WED1_URL_HERE',
   'Wednesday 2': 'YOUR_WED2_URL_HERE',
   'Thursday 1':  'https://script.google.com/macros/s/AKfycbztaebaWfOeszc6f7ukykQHUO6ysUWko5Zsttai-CUfoadM6sWQIGkBiXp1-qeQwXwV/exec',
-  'Thursday 2':  'https://script.google.com/macros/s/AKfycbyKD30xjK9MIVmW_iNg-uqaqr6X1-i6e880ev5Gy-zhRWZ1JNbTSyjxrwLwPmzK1aHS/exec',
+  'Thursday 2':  'https://script.google.com/macros/s/AKfycbwHmYe3VrRV-MOvngfBfgkm0kEEEo8AYUuRvXCbkeF4BksEwqrEyOZjSbgSgavG3Dpo/exec',
   'Friday 1':    'YOUR_FRI1_URL_HERE',
   'Friday 2':    'YOUR_FRI2_URL_HERE'
 };
