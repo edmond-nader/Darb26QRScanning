5eede00ccbd7ad39f7f4260deedb9a1461250ba2
edmond-nader <49365047+edmond-nader@users.noreply.github.com>
2026-04-03 15:14:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ var DAY_URLS = {
   'Wednesday 2': 'YOUR_WED2_URL_HERE',
   'Thursday 1':  'https://script.google.com/macros/s/AKfycbztaebaWfOeszc6f7ukykQHUO6ysUWko5Zsttai-CUfoadM6sWQIGkBiXp1-qeQwXwV/exec',
   'Thursday 2':  'https://script.google.com/macros/s/AKfycbwHmYe3VrRV-MOvngfBfgkm0kEEEo8AYUuRvXCbkeF4BksEwqrEyOZjSbgSgavG3Dpo/exec',
-  'Friday 1':    'https://script.google.com/macros/s/AKfycbzFu2oM0P5gLsYH5tluzPfk_kPVr0os9o75p6K3-yw6Pqz1xQ8MUyxe5_gQqUy5GJF5dA/exec',
-  'Friday 2':    'https://script.google.com/macros/s/AKfycbyo1fBXoTuwTyr-GT_ysQ8cUQXn95BEbZGKIrQiKPDTs7MyI9Q5VTPtHJrmuwqhkiQQ9w/exec'
+  'Friday 1':    'https://script.google.com/macros/s/AKfycbzelvYnrw2maX12n0Q0vSESg1oR4y6r6y14XR6z9h3ymKothTuxAuOOWwWmmpqWlTZ78w/exec',
+  'Friday 2':    'https://script.google.com/macros/s/AKfycbzCJvTxwEWomCZaMwckThs9Vgaka_kqIP5biJhkLtkBwy0Y7f980EtWOxnEDLqmSn9umw/exec'
 };
